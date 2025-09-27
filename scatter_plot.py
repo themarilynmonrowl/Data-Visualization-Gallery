b1cd9d74c5b9bdde441d33ba2b8cd486895b1e2d
Marilyn <108523423+themarilynmonrowl@users.noreply.github.com>
2025-09-27 16:30:49 +0100

    Create scatter_plot.py

---
+++ scatter_plot.py
@@ -0,0 +1,13 @@
+## Scatter Plot: Age vs Height ##
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# use penguins dataset from seaborn library
+df = sns.load_dataset("penguins").dropna()
+
+plt.figure(figsize=(8, 6))
+plt.scatter(df["flipper_length_mm"], df["body_mass_g"], c="purple", alpha=0.6, edgecolors="k")
+plt.title("Scatter Plot: Flipper Length vs Body Mass")
+plt.xlabel("Flipper Length (mm)")
+plt.ylabel("Body Mass (g)")
+plt.show()
