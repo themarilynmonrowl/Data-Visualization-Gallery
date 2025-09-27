76caaf56112272aea3288c64108719861edf6fdb
Marilyn <108523423+themarilynmonrowl@users.noreply.github.com>
2025-09-27 19:18:49 +0100

    Update scatter_plot.py

--- scatter_plot.py
+++ scatter_plot.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 # use penguins dataset from seaborn library
 df = sns.load_dataset("penguins").dropna()
 
+# plot the figure
 plt.figure(figsize=(8, 6))
 plt.scatter(df["flipper_length_mm"], df["body_mass_g"], c="purple", alpha=0.6, edgecolors="k")
 plt.title("Scatter Plot: Flipper Length vs Body Mass")
