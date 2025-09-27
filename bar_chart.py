d5e372b3d92c208a9541c06ab0cd2d96ff9fbcdb
Marilyn <108523423+themarilynmonrowl@users.noreply.github.com>
2025-09-27 16:32:37 +0100

    Create bar_chart.py

---
+++ bar_chart.py
@@ -0,0 +1,14 @@
+## Flights per Airline Company ##
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = sns.load_dataset("flights")
+flights_per_year = df.groupby("year")["passengers"].sum()
+
+plt.figure(figsize=(8, 6))
+flights_per_year.plot(kind="bar", color="skyblue", edgecolor="k")
+plt.title("Bar Chart: Total Flights per Year")
+plt.xlabel("Year")
+plt.ylabel("Passengers")
+plt.show()
